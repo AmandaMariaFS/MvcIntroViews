559be87fdb1df3e8467f1b49cbf67136b871aac3
AmandaMariaFS <amandinhaferre@hotmail.com>
2022-09-25 18:40:24 -0300

    Implementacao do resto do exercicio

--- Controllers/OperacoesController.cs
+++ Controllers/OperacoesController.cs
@@ -19,4 +19,46 @@ public class OperacoesController : Controller
 
         return View();
     }
+
+    public IActionResult Subtrair()
+    {
+        return View();
+    }
+
+    public IActionResult ResultadoSubtracao(double n1, double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 - n2;
+
+        return View();
+    }
+
+    public IActionResult Multiplicar()
+    {
+        return View();
+    }
+
+    public IActionResult ResultadoMultiplicacao(double n1, double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 * n2;
+
+        return View();
+    }
+
+    public IActionResult Dividir()
+    {
+        return View();
+    }
+
+    public IActionResult ResultadoDivisao(double n1, double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1/n2;
+
+        return View();
+    }
 }
